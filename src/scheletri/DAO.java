a911d19a8c0247f731926961a81d78d3e977b967
Utente <Utente@10.32.3.252>
2019-06-27 15:16:15 +0200

    scheletro

--- src/scheletri/DAO.java
+++ src/scheletri/DAO.java
@@ -31,7 +31,23 @@ public class DAO {
 	}
 		
 	}
-
+	SELECT e.offense_type_id,e2.offense_type_id,COUNT(DISTINCT e.district_id)
+	FROM events e,events e2
+	WHERE YEAR(e.reported_date)=2014 AND YEAR(e2.reported_date)=2014 
+	AND e.district_id=e2.district_id
+	AND e.offense_category_id='larceny' AND e2.offense_category_id='larceny'
+	AND e.offense_type_id>e2.offense_type_id
+	GROUP BY  e.offense_type_id,e2.offense_type_id
+	
+	
+	
+	SELECT e.offense_type_id,e2.offense_type_id,COUNT(DISTINCT e.neighborhood_id)
+	FROM events e,events e2
+	WHERE MONTH(e.reported_date)=3 AND MONTH(e2.reported_date)=3 
+	AND e.neighborhood_id=e2.neighborhood_id
+	AND e.offense_category_id='auto-theft' AND e2.offense_category_id='auto-theft'
+	AND e.offense_type_id>e2.offense_type_id
+	GROUP BY  e.offense_type_id,e2.offense_type_id
 	
 	
 }
